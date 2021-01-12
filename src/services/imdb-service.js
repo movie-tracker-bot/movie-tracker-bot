96d9b3a64486cc87863773979833569dd29f1f20
Fran <franciellyneves@dcc.ufmg.br>
2021-01-12 18:06:17 -0300

    Initial code with imdb integration

---
+++ src/services/imdb-service.js
@@ -0,0 +1,43 @@
+const axios = require('axios')
+
+class ImdbService {
+
+    async getMovieByTitle(title) {
+        try {
+            let response = await axios.get('https://imdb8.p.rapidapi.com/title/find', {
+                params: {
+                    q: title
+                },
+                timeout: 5000,
+                headers: {
+                    'x-rapidapi-key': '73c8548501mshc421236698fb7a2p1d9497jsndc5877340195',
+                    'x-rapidapi-host': 'imdb8.p.rapidapi.com'
+                }
+            })
+            return response.data.data
+        } catch (err) {
+            Log.warn('Erro ao recuperar o nome do filme')
+            return null
+        }
+    }
+
+    async getMovieDetails(id) {
+        try {
+            let response = await axios.get('https://imdb8.p.rapidapi.com/title/get-details', {
+                params: {
+                    tconst: id
+                },
+                timeout: 5000,
+                headers: {
+                    'x-rapidapi-key': '73c8548501mshc421236698fb7a2p1d9497jsndc5877340195',
+                    'x-rapidapi-host': 'imdb8.p.rapidapi.com'
+                }
+            })
+            return response.data.data
+        } catch (err) {
+            Log.warn('Erro ao recuperar os dados detalhados do filme')
+            return null
+        }
+    }
+}
+module.exports = ImdbService
